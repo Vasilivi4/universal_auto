0afa6b125e46cff962ca0e3065f267a49315e2f1
Vasilivich <137120278+Vasilivi4@users.noreply.github.com>
2025-06-19 00:20:00 +0300

    Update test_fleet.py

--- app/tests/models_test/test_fleet.py
+++ app/tests/models_test/test_fleet.py
@@ -4,9 +4,9 @@ from app.models import Fleet
 
 # Check write data to Fleet model
 @pytest.mark.django_db
-@pytest.mark.parametrize('fleet_name, fees', [("Uber", 0.50),
-                                             ("Bolt", 0.50),
-                                             ("Uklon", 0.50)])
+@pytest.mark.parametrize(
+    "fleet_name, fees", [("Uber", 0.50), ("Bolt", 0.50), ("Uklon", 0.50)]
+)
 def test_fleets_create(fleet_name, fees):
     fleet = Fleet.objects.create(name=fleet_name, fees=fees)
     assert fleet.name == fleet_name
