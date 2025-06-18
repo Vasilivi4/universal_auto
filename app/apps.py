f15c7d6e82e8bdaeed29d6947371834be3e0d202
Vasilivich <137120278+Vasilivi4@users.noreply.github.com>
2025-06-19 00:18:55 +0300

    Update apps.py

--- app/apps.py
+++ app/apps.py
@@ -2,9 +2,12 @@ from django.apps import AppConfig
 
 
 class AppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app'
-    verbose_name = 'Ninja Taxi'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "app"
+    verbose_name = "Ninja Taxi"
 
     def ready(self):
-        import app.signals
+        import app.signals  # noqa: F401  # Import for side effects (
+
+
+# Django signals)
