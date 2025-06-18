5b449bae726fd8e582dc88667f54b1d4727c38d9
Vasilivich <137120278+Vasilivi4@users.noreply.github.com>
2025-06-19 00:20:40 +0300

    Update test_user.py

--- app/tests/models_test/test_user.py
+++ app/tests/models_test/test_user.py
@@ -3,12 +3,23 @@ from app.models import User
 
 
 @pytest.mark.django_db
-@pytest.mark.parametrize('email', 'phone_number', 'chat_id', [("admin@admin.com", "+380964525445", "123"),
-                                                              ("admin1@admin.com", "+380964234445", "312"),
-                                                              ("admin2@admin.com", "+380964525412", "213")])
+@pytest.mark.parametrize(
+    "email",
+    "phone_number",
+    "chat_id",
+    [
+        ("admin@admin.com", "+380964525445", "123"),
+        ("admin1@admin.com", "+380964234445", "312"),
+        ("admin2@admin.com", "+380964525412", "213"),
+    ],
+)
 def test_users_create(email, phone_number, chat_id):
 
-    user = User.objects.create(email=email, phone_number=phone_number, chat_id=chat_id)
+    user = User.objects.create(
+        email=email,
+        phone_number=phone_number,
+        chat_id=chat_id
+    )
     assert user.email == email
     assert user.phone_number == phone_number
     assert user.chat_id == chat_id
@@ -23,4 +34,3 @@ def test_get_by_chat_id():
 @pytest.mark.django_db
 def test_fill_deleted_at_by_number():
     assert fill_deleted_at_by_number("+380964525445").deleted_at != null
-
