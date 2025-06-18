a1cab6df45f69d14f801e7d5a8b82b61b121ce70
Vasilivich <137120278+Vasilivi4@users.noreply.github.com>
2025-06-19 00:19:33 +0300

    Update test_b_payments_order.py

--- app/tests/models_test/test_b_payments_order.py
+++ app/tests/models_test/test_b_payments_order.py
@@ -7,7 +7,10 @@ def test_bolt_payments_order(db) -> BoltPaymentsOrder:
     return BoltPaymentsOrder.objects.create(
         report_from='2022-08-29+03:00',
         report_to='2022-09-04+03:00',
-        report_file_name='Щотижневий звіт Bolt – 2022W35 – Kyiv Fleet 03_232 park Universal-auto.csv',
+        report_file_name=(
+            'Щотижневий звіт Bolt – 2022W35 – Kyiv Fleet 03_232 park '
+            'Universal-auto.csv'
+        ),
         driver_full_name="Анатолій Мухін",
         mobile_number="+380936503350",
         range_string="Тиждень 2022-08-29 - 2022-09-04",
@@ -29,10 +32,15 @@ def test_bolt_payments_order(db) -> BoltPaymentsOrder:
 def test_db(test_bolt_payments_order):
     assert test_bolt_payments_order.report_from == '2022-08-29+03:00'
     assert test_bolt_payments_order.report_to == '2022-09-04+03:00'
-    assert test_bolt_payments_order.report_file_name == 'Щотижневий звіт Bolt – 2022W35 – Kyiv Fleet 03_232 park Universal-auto.csv'
+    assert test_bolt_payments_order.report_file_name == (
+        'Щотижневий звіт Bolt – 2022W35 – Kyiv Fleet 03_232 park '
+        'Universal-auto.csv'
+    )
     assert test_bolt_payments_order.driver_full_name == "Анатолій Мухін"
     assert test_bolt_payments_order.mobile_number == "+380936503350"
-    assert test_bolt_payments_order.range_string == "Тиждень 2022-08-29 - 2022-09-04"
+    assert test_bolt_payments_order.range_string == (
+        "Тиждень 2022-08-29 - 2022-09-04"
+    )
     assert test_bolt_payments_order.total_amount == float(7233.00)
     assert test_bolt_payments_order.cancels_amount == float(0.00)
     assert test_bolt_payments_order.autorization_payment == float(0.00)
@@ -53,20 +61,49 @@ def test_driver_id(test_bolt_payments_order):
 
 
 def test_total_cach_less_drivers_amount(test_bolt_payments_order):
-    assert test_bolt_payments_order.total_amount + (test_bolt_payments_order.fee) + test_bolt_payments_order.cancels_amount + test_bolt_payments_order.driver_bonus == 5424.75
+    assert (
+        test_bolt_payments_order.total_amount
+        + test_bolt_payments_order.fee
+        + test_bolt_payments_order.cancels_amount
+        + test_bolt_payments_order.driver_bonus
+        == 5424.75
+    )
 
 
 def test_kassa(test_bolt_payments_order):
-    assert test_bolt_payments_order.total_amount + (test_bolt_payments_order.fee) + test_bolt_payments_order.cancels_amount + test_bolt_payments_order.driver_bonus == 5424.75
+    assert (
+        test_bolt_payments_order.total_amount
+        + test_bolt_payments_order.fee
+        + test_bolt_payments_order.cancels_amount
+        + test_bolt_payments_order.driver_bonus
+        == 5424.75
+    )
 
 
 def test_total_drivers_amount(test_bolt_payments_order, rate=0.65):
-    assert round(((test_bolt_payments_order.total_amount + (test_bolt_payments_order.fee) + test_bolt_payments_order.cancels_amount + test_bolt_payments_order.driver_bonus) * rate + test_bolt_payments_order.total_amount_cach), 2) == -403.91
+    assert round(
+        (
+            test_bolt_payments_order.total_amount
+            + test_bolt_payments_order.fee
+            + test_bolt_payments_order.cancels_amount
+            + test_bolt_payments_order.driver_bonus
+        ) * rate
+        + test_bolt_payments_order.total_amount_cach,
+        2
+    ) == -403.91
 
 
 def test_total_owner_amount(test_bolt_payments_order, rate=0.65):
     # 771.45 its result test_total_drivers_amount with rate=0.65
-    assert round(((test_bolt_payments_order.total_amount + (test_bolt_payments_order.fee) + test_bolt_payments_order.cancels_amount + test_bolt_payments_order.driver_bonus) * (1-rate) - 771.45), 2) == 1127.21
+    assert round(
+        (
+            test_bolt_payments_order.total_amount
+            + test_bolt_payments_order.fee
+            + test_bolt_payments_order.cancels_amount
+            + test_bolt_payments_order.driver_bonus
+        ) * (1 - rate) - 771.45,
+        2
+    ) == 1127.21
 
 
 def test_field_length(test_bolt_payments_order):
