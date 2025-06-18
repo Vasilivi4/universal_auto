71fd63766928fd2ed2bb35eac25971b5a6c7b09d
Vasilivich <137120278+Vasilivi4@users.noreply.github.com>
2025-06-19 00:19:42 +0300

    Update test_bolt_transactions.py

--- app/tests/models_test/test_bolt_transactions.py
+++ app/tests/models_test/test_bolt_transactions.py
@@ -7,21 +7,23 @@ class BoltTransactionsTests(TestCase):
 
     @transaction.atomic
     def create_spam(self):
-        spam = BoltTransactions.objects.create(driver_name='Юрій Філіппов',
-                                               driver_number='+380502428878',
-                                               trip_date='2022-09-12',
-                                               payment_confirmed='19:03',
-                                               boarding='Івана Дяченка вулиця 13, Киев 02088',
-                                               payment_method='Готівка',
-                                               requsted_time='18:06',
-                                               fare=283.00,
-                                               payment_authorization=0.00,
-                                               service_tax=0.00,
-                                               cancel_payment=0.00,
-                                               tips=0.00,
-                                               order_status='Завершено',
-                                               car='Renault Mégane',
-                                               license_plate='KA6047EI')
+        spam = BoltTransactions.objects.create(
+            driver_name="Юрій Філіппов",
+            driver_number="+380502428878",
+            trip_date="2022-09-12",
+            payment_confirmed="19:03",
+            boarding=("Івана Дяченка вулиця 13, Киев 02088"),
+            payment_method="Готівка",
+            requsted_time="18:06",
+            fare=283.00,
+            payment_authorization=0.00,
+            service_tax=0.00,
+            cancel_payment=0.00,
+            tips=0.00,
+            order_status="Завершено",
+            car="Renault Mégane",
+            license_plate="KA6047EI",
+        )
         spam.save()
         return True
 
@@ -32,4 +34,3 @@ class BoltTransactionsTests(TestCase):
         self.assertRaises(IntegrityError, self.create_spam)
         # have we recovered?
         self.assertEquals(len(BoltTransactions.objects.all()), 1)
-
